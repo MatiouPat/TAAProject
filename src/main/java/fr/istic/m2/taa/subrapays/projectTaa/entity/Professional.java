eee4e956a866286c20afa892bf90f79b677943b9
Matthieu Pays <pays.matthieuic@gmail.com>
2021-11-05 15:37:06 +0100

    Temporary base for the frontend

--- src/main/java/fr/istic/m2/taa/subrapays/projectTaa/entity/Professional.java
+++ src/main/java/fr/istic/m2/taa/subrapays/projectTaa/entity/Professional.java
@@ -9,7 +9,7 @@ import java.util.Collection;
 public class Professionnal extends User
 {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String profession;
